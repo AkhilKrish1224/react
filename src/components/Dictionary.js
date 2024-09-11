2d406d8008757e57f92c4b8e92b92cd4a0a6f01b
AkhilKrish1224 <akhilkrishnan1224@gmail.com>
2024-09-11 18:49:08 +0530

    Basic useEffect practice

---
+++ src/components/Dictionary.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+export default function Dictionary() {
+  const [word, setWord] = useState("");
+  const [word2, setWord2] = useState("");
+
+  useEffect(() => {
+    console.log("State Updated " + word);
+  }, [word]);
+
+  useEffect(() => {
+    console.log("State Updated " + word2);
+  }, [word2]);
+
+  return (
+    <>
+      <input
+        type="text"
+        onChange={(e) => {
+          setWord(e.target.value);
+        }}
+      />
+      <h1>Lets get the definition for {word}</h1>
+      <input
+        type="text"
+        onChange={(e) => {
+          setWord2(e.target.value);
+        }}
+      />
+      <h1>Lets get the definition for {word2}</h1>
+    </>
+  );
+}
