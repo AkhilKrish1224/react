a860fe5f7c2f2cf3c02a29cd6a9884004359c93b
AkhilKrish1224 <akhilkrishnan1224@gmail.com>
2024-09-12 23:13:10 +0530

    Added Customer NotFound

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -1,19 +1,30 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { baseUrl } from "../shared";
+import NotFound from "../components/NotFound";
 
 export default function Customer() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [customer, setCustomer] = useState();
+  const [notFound, setNotFound] = useState(false);
   useEffect(() => {
-    const url = "http://localhost:8000/api/customers/" + id;
+    const url = baseUrl + "api/customers/" + id;
     fetch(url)
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.status === 404) {
+          //navigate("/404");
+          setNotFound(true);
+        }
+        return response.json();
+      })
       .then((data) => {
         setCustomer(data.customer);
       });
   }, []);
   return (
     <>
+      {notFound ? <p>The customer with id {id} was not found.</p> : null}
       {customer ? (
         <div>
           <p>{customer.id}</p>
