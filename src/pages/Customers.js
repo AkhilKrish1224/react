88084bd3ad80771a1e93e42882ee8ac3fb7b922f
AkhilKrish1224 <akhilkrishnan1224@gmail.com>
2024-09-12 21:53:39 +0530

    Consume Backend API

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,3 +1,23 @@
+import { useEffect, useState } from "react";
 export default function Customers() {
-  return <h1>Hello There!!</h1>;
+  const [customers, setCustomers] = useState();
+  useEffect(() => {
+    console.log("fetching...");
+    fetch("http://localhost:8000/api/customers/")
+      .then((response) => response.json())
+      .then((data) => {
+        setCustomers(data.customers);
+        console.log(data);
+      });
+  }, []);
+  return (
+    <>
+      <h1>Here are our Customers: </h1>
+      {customers
+        ? customers.map((customer) => {
+            return <p>{customer.name}</p>;
+          })
+        : null}
+    </>
+  );
 }
