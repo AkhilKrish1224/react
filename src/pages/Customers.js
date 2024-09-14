ddc8a8b92146bba2ac82fd65b064694dc227c3a6
AkhilKrish1224 <akhilkrishnan1224@gmail.com>
2024-09-14 17:59:51 +0530

    Added Form and Button CSS

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -5,6 +5,7 @@ import AddCustomer from "../components/AddCustomer";
 export default function Customers() {
   const [customers, setCustomers] = useState();
   const [show, setShow] = useState(false);
+  const [error, setError] = useState();
 
   function toggleShow() {
     setShow(!show);
@@ -14,10 +15,19 @@ export default function Customers() {
     console.log("fetching...");
     const url = baseUrl + "api/customers/";
     fetch(url)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+        return response.json();
+      })
       .then((data) => {
         setCustomers(data.customers);
         console.log(data);
+        setError(undefined);
+      })
+      .catch((e) => {
+        setError(e.message);
       });
   }, []);
 
@@ -41,26 +51,30 @@ export default function Customers() {
         toggleShow();
         console.log(data);
         setCustomers([...customers, data.customer]);
+        setError(undefined);
       })
       .catch((e) => {
-        console.log(e);
+        setError(e.message);
       });
   }
 
   return (
     <>
       <h1>Here are our Customers: </h1>
-      <ul>
-        {customers
-          ? customers.map((customer) => {
-              return (
-                <li key={customer.id}>
-                  <Link to={"/customers/" + customer.id}>{customer.name}</Link>
-                </li>
-              );
-            })
-          : null}
-      </ul>
+      {customers
+        ? customers.map((customer) => {
+            return (
+              <div className="m-2" key={customer.id}>
+                <Link to={"/customers/" + customer.id}>
+                  <button className="no-underline bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded">
+                    {customer.name}
+                  </button>
+                </Link>
+              </div>
+            );
+          })
+        : null}
+      {error ? <p>{error}</p> : null}
       <AddCustomer
         newCustomer={newCustomer}
         show={show}
